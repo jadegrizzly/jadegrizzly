8b5c351d1f78cd8b85ebdd814a112d763b110982
Cory Asato <czasato@gmail.com>
2014-12-04 11:59:00 -0800

    added bootstrap and iron:router

---
+++ libs/router.js
@@ -0,0 +1,11 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+// Router.map(function() {
+//   this.route('home', { path: '/' });
+// });
+
+Router.route('/', function() {
+  this.render('Home');
+});
