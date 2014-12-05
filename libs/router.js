3f68157842c518a8ab578a0ec984e69cfff69a8a
Christian Perez <christian.manuel.perez@gmail.com>
2014-12-04 20:26:54 -0800

    Add view templates for menu, login, game creation; add routes to new views; modify styles

--- libs/router.js
+++ libs/router.js
@@ -5,3 +5,19 @@ Router.configure({
 Router.route('/', function() {
   this.render('home');
 });
+
+Router.route('/menu', function(){
+  this.render('menu');
+});
+
+Router.route('/create', function(){
+  this.render('create');
+});
+
+// Router.onBeforeAction(function() {
+//   if (! Meteor.userId()) {
+//     this.render('login');
+//   } else {
+//     this.next();
+//   }
+// });
