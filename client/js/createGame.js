a0e4b0249a078ebe874e7d8a8ed1ef8906168d85
Cory Asato <czasato@gmail.com>
2014-12-05 14:39:52 -0800

    working on events

--- client/js/createGame.js
+++ client/js/createGame.js
@@ -1,10 +1,11 @@
-Players = new Meteor.Collection('players');
-Games = new Meteor.Collection('games');
+// Players = new Meteor.Collection('players');
+// Games = new Meteor.Collection('games');
 
-Meteor.subscribe('players');
-Meteor.subscribe('users');
-Meteor.subscribe('games');
+// Meteor.subscribe('players');
+// Meteor.subscribe('users');
+// Meteor.subscribe('games');
 
+<<<<<<< HEAD
 Template.createGame.helpers({
   updateGameName: function(gameId, gameName) {
     Games.update(gameId, {$set: {gameName: gameName}});
@@ -14,4 +15,42 @@ Template.createGame.helpers({
     Meteor.call('gamesUpsert', gameId, {$push:{featList: featItem}});
   }
 
+<<<<<<< HEAD
 });
+=======
+
+Template.createGame.helpers({
+  verifyEvents: function(eventList) {
+    // check that it's an array
+    if (!Array.isArray(eventList) || eventList.length === 0) {
+      return false;
+    }
+
+    for (var i = 0; i < eventList.length; i++) {
+      if (!eventList[i] || !(eventName in eventList[i])) {
+        return false;
+      }
+    };
+
+    return true;
+  },
+
+  createGame: function(gameName, eventList) {
+    if (!verifyEvents(eventList)) {
+      return null;
+    }
+
+    var gameObj = {
+      gameName: "gameName", 
+      created_by: Meteor.userId(),
+      event_list: eventList,
+      participants: [Meteor.userId()]
+    };
+
+    Games.insert(gameObj, function(err, id) {
+      Meteor.call('playersUpsert', Meteor.userId(), {$push:{'gameList':id}});
+    });
+  }
+};
+
+>>>>>>> working on events
