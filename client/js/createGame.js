a72ac212cfa5409c8b2f8947b1ae9a956a5558d8
Cory Asato <czasato@gmail.com>
2014-12-05 15:22:52 -0800

    fixing merge

--- client/js/createGame.js
+++ client/js/createGame.js
@@ -15,39 +15,3 @@ Template.createGame.helpers({
   }
 });
 
-
-Template.createGame.helpers({
-  verifyEvents: function(eventList) {
-    // check that it's an array
-    if (!Array.isArray(eventList) || eventList.length === 0) {
-      return false;
-    }
-
-    for (var i = 0; i < eventList.length; i++) {
-      if (!eventList[i] || !(eventName in eventList[i])) {
-        return false;
-      }
-    };
-
-    return true;
-  },
-
-  createGame: function(gameName, eventList) {
-    if (!verifyEvents(eventList)) {
-      return null;
-    }
-
-    var gameObj = {
-      gameName: "gameName", 
-      created_by: Meteor.userId(),
-      event_list: eventList,
-      participants: [Meteor.userId()]
-    };
-
-    Games.insert(gameObj, function(err, id) {
-      Meteor.call('playersUpsert', Meteor.userId(), {$push:{'gameList':id}});
-    });
-  }
-};
-
-
--- libs/router.js
+++ libs/router.js
@@ -15,35 +15,18 @@ Router.route('/menu', function(){
 });
 
 Router.route('/create', function(){
-  this.render('createGame');
+  this.render('create');
 });
 
 Router.route('/game', function(){
   this.render('game');
 });
 
-<<<<<<< HEAD
 Router.route('/photos', function(){
   this.render('photos');
 });
 
 AccountsTemplates.configureRoute('signIn');
-=======
-Accounts.onLogin(function(){
-  console.log('Login success');
-  Router.render('menu');
-});
-
-
-
-Router.onBeforeAction(function() {
-  if (! Meteor.userId()) {
-    this.render('layout');
-  } else {
-    this.next();
-  }
-});
->>>>>>> working on sessions
 
 AccountsTemplates.configureRoute('signIn', {
     name: 'signin',
