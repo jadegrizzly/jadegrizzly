4ef9109bd27a87c30a2ffb868bdd2a4412146104
Cory Asato <czasato@gmail.com>
2014-12-06 17:26:10 -0800

    working on game view

--- client/js/game.js
+++ client/js/game.js
@@ -1,5 +1,6 @@
 Template.game.helpers({
   newGame: function() {
-    console.log(Session.get('currentGameId'));
+    console.log(Games.findOne(Session.get('currentGameId'), {"gameName": 1}));
+    return Games.findOne(Session.get('currentGameId'), {"gameName": 1});
   }
 });
