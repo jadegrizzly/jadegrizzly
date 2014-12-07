86c215982c9ab04882a152bf875dfb3d546a9abb
Cory Asato <czasato@gmail.com>
2014-12-06 17:45:45 -0800

    game view linked to DB

--- client/js/game.js
+++ client/js/game.js
@@ -1,6 +1,11 @@
 Template.game.helpers({
   newGame: function() {
-    console.log(Games.findOne(Session.get('currentGameId'), {"gameName": 1}));
-    return Games.findOne(Session.get('currentGameId'), {"gameName": 1});
+    return Games.findOne(Session.get('currentGameId'));
+  },
+
+  events: function() {
+    var q = Games.findOne(Session.get('currentGameId'));
+    return q.featList;
   }
 });
+
