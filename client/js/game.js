538858906cd4e9c4665643a87436d65275dfa8cc
Christian Perez <christian.manuel.perez@gmail.com>
2014-12-07 11:29:23 -0800

    Add back and logout buttons to game view

--- client/js/game.js
+++ client/js/game.js
@@ -28,6 +28,17 @@ Template.gameEvent.events({
       
       Meteor.call('featListUpdate', {_id: gameId, 'featList.name': featName }, {$push: {"featList.$.competedBy" : {"playerId":userId, "photoURL":data}}});
     });
+  },
+
+  'click .go-back': function(evt, template){
+    Router.go('/menu');
+  },
+
+  'click .logout': function(evt, template){
+    console.log('Logging user out...');
+    Meteor.logout(function(err) {
+      Router.go('/');
+    });
   }
 });
 
