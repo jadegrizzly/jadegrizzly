750fae541cc70ddd7d876adcd620d731ddc24ef5
Cory Asato <czasato@gmail.com>
2014-12-08 09:28:50 -0800

    Created a modular environment

---
+++ client/homeView/index.js
@@ -0,0 +1,41 @@
+/**
+ * Setup Client Collections
+ */
+
+Players = new Meteor.Collection('players');
+Games = new Meteor.Collection('games');
+Feats = new Meteor.Collection('feats');
+
+Meteor.subscribe('players');
+Meteor.subscribe('users');
+Meteor.subscribe('games');
+
+/**
+ * Logs to Template
+ */
+
+Template.registerHelper('log', function(something) {
+  console.log(something);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
